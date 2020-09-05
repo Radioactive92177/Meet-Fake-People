5f8d2dc647eff3bb1074d6a41442dd322caa5863
Radioactive92177 <Raju.11811924@lpu.in>
2020-09-06 03:13:55 +0530

    Fake Data generator made

--- assets/js/fakeData.js
+++ assets/js/fakeData.js
@@ -7,7 +7,11 @@ class FakeData {
           name : this.faker.name.findName(),
           age : Math.ceil(Math.random()* 30 + 18),
           gender : this.faker.name.gender(),
-          lookingFor : this.faker.name.gender()
+          lookingFor : this.faker.name.gender(),
+          image : this.faker.image.people()
       }
   }
 }
+
+const data = new FakeData;
+console.log(data.getFakeData())
